75374a8acd939deb6f044436d660379cb12667c1
Quinn Unger <quinn.unger@pm.me>
2026-05-22 12:34:35 +0100

    add hs_register

--- src/handshakes/handshakes/rtl/hs_buffer.sv
+++ src/handshakes/handshakes/rtl/hs_buffer.sv
@@ -1,10 +1,6 @@
-`include "hs_macro.sv"
-
 module hs_buffer (
     hs_io.flw flw_hs,
     hs_io.ldr ldr_hs
 );
-    // functionally equivalent
-    `HS_ASSERT_H(flw_hs, ldr_hs)
-    hs_absorb_aborts hs_absorb_aborts (.*);
+    hs_register #(.AbsorbAborts(1'b0)) hs_register (.*);
 endmodule : hs_buffer
