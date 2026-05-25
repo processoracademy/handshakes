0ac63f4da33a3d1dbdd35524b9d1d09895215c70
Quinn Unger <quinn.unger@pm.me>
2026-05-25 13:48:17 +0100

    rewrite hs_serialize to take length on wide input

--- src/handshakes/handshakes/rtl/hs_serialize.sv
+++ src/handshakes/handshakes/rtl/hs_serialize.sv
@@ -1,38 +1,57 @@
 `include "hs_macro.sv"
 module hs_serialize #(
-    parameter logic BigEndian    = 1'b0,
-    parameter logic AbsorbAborts = 1'b0
+    parameter integer unsigned WideW        = 0,
+    parameter logic            BigEndian    = 1'b0,
+    parameter logic            AbsorbAborts = 1'b0
 ) (
-    hs_io.flw wide_hs,
-    hs_io.ldr narrow_hs
+           hs_io.ldr                                                         narrow_hs,
+    input  logic        [          (WideW/narrow_hs.W)-1:0][narrow_hs.W-1:0] data_i,
+    input  logic        [$clog2((WideW/narrow_hs.W)+1)-1:0]                  length_i,
+    input  hs::fprobe_s                                                      fprobe_i,
+    output hs::fdrv_s                                                        fdrv_o
 );
-    wire clk = wide_hs.clk;
-    wire clk_en = wide_hs.clk_en;
-    wire sync_rst = wide_hs.sync_rst;
+    wire clk = narrow_hs.clk;
+    wire clk_en = narrow_hs.clk_en;
+    wire sync_rst = narrow_hs.sync_rst;
 
     initial begin
-        assert ((wide_hs.W / narrow_hs.W) * narrow_hs.W == wide_hs.W)
-        else $fatal(1, "wide_hs width %0d must be a multiple of narrow_hs width %0d", wide_hs.W, narrow_hs.W);
-        assert (wide_hs.W > narrow_hs.W)
-        else $fatal(1, "wide_hs width %0d must be greater than narrow_hs width %0d", wide_hs.W, narrow_hs.W);
+        assert ((WideW / narrow_hs.W) * narrow_hs.W == WideW)
+        else $fatal(1, "wide_hs width %0d must be a multiple of narrow_hs width %0d", WideW, narrow_hs.W);
+        assert (WideW > narrow_hs.W)
+        else $fatal(1, "wide_hs width %0d must be greater than narrow_hs width %0d", WideW, narrow_hs.W);
     end
 
-    localparam integer Slices = wide_hs.W / narrow_hs.W;
+    localparam integer Slices = WideW / narrow_hs.W;
     typedef logic [$clog2(Slices)-1:0] ptr_t;
-    typedef logic [Slices-1:0][narrow_hs.W-1:0] wide_t;
+    typedef type (data_i) wide_t;
+    typedef type (length_i) length_t;
 
-    hs_io #(.T(wide_t)) sliced_hs (.*);
-    hs_replace_data hs_replace_data_sliced (
+    typedef struct packed {
+        wide_t   data;
+        length_t length;
+    } wide_s;
+
+    hs_io #(.T(wide_s)) wide_hs (.*);
+    assign wide_hs.ldrv.req    = fprobe_i.req;
+    assign wide_hs.ldrv.last   = fprobe_i.last;
+    assign fdrv_o.ack          = wide_hs.fdrv.ack;
+    assign wide_hs.data.data   = data_i;
+    assign wide_hs.data.length = length_i;
+
+    hs_io #(.T(wide_s)) filtered_hs (.*);
+    wire length_gt_0 = wide_hs.data.length > length_t'(0);
+    hs_filter hs_filter_zero_length (
         .flw_hs(wide_hs),
-        .ldr_hs(sliced_hs),
-        .data_i(wide_t'(wide_hs.data))
+        .ldr_hs(filtered_hs),
+        .pass_i(length_gt_0),
+        .drop_o()
     );
 
-    hs_io #(.T(wide_t)) register_hs (.*);
+    hs_io #(.T(wide_s)) register_hs (.*);
     hs_register #(
         .AbsorbAborts(AbsorbAborts)
     ) hs_register (
-        .flw_hs(sliced_hs),
+        .flw_hs(filtered_hs),
         .ldr_hs(register_hs)
     );
 
@@ -41,8 +60,9 @@ module hs_serialize #(
 
     ptr_t ptr;
 
-    localparam ptr_t PtrReset = BigEndian ? ptr_t'(Slices - 1) : '0;
-    wire ptr_end = ptr == (BigEndian ? '0 : ptr_t'(Slices - 1));
+    localparam length_t WordsMax = length_t'(Slices);
+    localparam ptr_t PtrReset = BigEndian ? ptr_t'(WordsMax - length_t'(1)) : '0;
+    wire ptr_end = ptr == (BigEndian ? ptr_t'(WordsMax - register_hs.data.length) : ptr_t'(register_hs.data.length - length_t'(1)));
 
     always_comb begin
         unique case (register_hs.state)
@@ -78,7 +98,7 @@ module hs_serialize #(
 
     hs::lctl_s lctl;
     assign narrow_hs.ldrv = hs::drive_ldr(narrow_hs.state, lctl);
-    assign narrow_hs.data = `HS_CAST(narrow_hs, register_hs.data[ptr]);
+    assign narrow_hs.data = `HS_CAST(narrow_hs, register_hs.data.data[ptr]);
     assign lctl.start     = valid;
     assign lctl.pause     = !valid;
     assign lctl.close     = ptr_end && last && valid;
