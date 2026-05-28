c4cc829c786580d1eb5951c3b3af8d3806064b51
Quinn Unger <quinn.unger@pm.me>
2026-05-28 12:15:25 +0100

    fix type error in `hs_reframe`

--- src/handshakes/handshakes/rtl/hs_reframe.sv
+++ src/handshakes/handshakes/rtl/hs_reframe.sv
@@ -14,7 +14,7 @@ module hs_reframe (
     typedef logic [flw_hs.W-1:0] data_t;
 
     hs_io #(.T(data_t)) internal_hs (.*);
-    hs_replace_data hs_replace_data (
+    hs_replace_data hs_replace_data_internal (
         .flw_hs(request_hs),
         .ldr_hs(internal_hs),
         .data_i(data_t'(flw_hs.data))
@@ -23,10 +23,16 @@ module hs_reframe (
     assign fctl.ready = flw_hs.ldrv.req;
     assign fctl.pause = !flw_hs.ldrv.req;
     assign fctl.block = 1'b0;
+    hs_io #(.T(data_t)) ldr_deref_hs (.*);
     hs_override_fctl hs_override_fctl (
         .flw_hs(internal_hs),
-        .ldr_hs(ldr_hs),
+        .ldr_hs(ldr_deref_hs),
         .fctl_i(fctl)
+    );
+    hs_replace_data hs_replace_data_ldr (
+        .flw_hs(ldr_deref_hs),
+        .ldr_hs(ldr_hs),
+        .data_i(ldr_deref_hs.data)
     );
 
     assign flw_hs.fdrv.ack = internal_hs.ldrv.req && ldr_hs.fdrv.ack && (ldr_hs.state != hs::BLOCK) && (flw_hs.state != hs::BLOCK);
