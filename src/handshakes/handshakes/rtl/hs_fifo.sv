d69a0e4b2dc1d5e0b0168aede2f401930845d630
Quinn Unger <quinn.unger@pm.me>
2026-06-10 13:33:27 +0100

    remove abort propagation from `hs_fifo`

--- src/handshakes/handshakes/rtl/hs_fifo.sv
+++ src/handshakes/handshakes/rtl/hs_fifo.sv
@@ -1,8 +1,7 @@
 `include "hs_macro.sv"
 module hs_fifo #(
-    parameter integer Depth        = 2,
-    parameter logic   SingleFrame  = 1'b0,
-    parameter logic   BufferAborts = 1'b1
+    parameter integer Depth       = 2,
+    parameter logic   SingleFrame = 1'b0
 ) (
     hs_io.flw flw_hs,
     hs_io.ldr ldr_hs
@@ -13,16 +12,15 @@ module hs_fifo #(
     wire clk_en = flw_hs.clk_en;
     wire sync_rst = flw_hs.sync_rst;
 
-    localparam integer SafeDepth = BufferAborts ? (Depth + 1) : Depth;
-    localparam integer SafePtrWidth = (SafeDepth > 1) ? $clog2(SafeDepth) : 1;
+    localparam integer SafePtrWidth = (Depth > 1) ? $clog2(Depth) : 1;
 
     typedef logic [flw_hs.W-1:0] data_t;
     typedef logic [SafePtrWidth-1:0] ptr_t;
-    typedef logic [$clog2(SafeDepth+1)-1:0] util_t;
+    typedef logic [$clog2(Depth+1)-1:0] util_t;
 
-    localparam ptr_t AddrMax = ptr_t'(SafeDepth - 1);
+    localparam ptr_t AddrMax = ptr_t'(Depth - 1);
 
-    localparam integer EntryWidth = flw_hs.W + 1 + 32'(BufferAborts);
+    localparam integer EntryWidth = flw_hs.W + 1;
     typedef logic [EntryWidth-1:0] entry_t;
 
     logic      advance_ldr;
@@ -60,78 +58,43 @@ module hs_fifo #(
             assign internal_fctl.block = 1'b0;
         end
 
-        if (BufferAborts) begin : g_preserve_aborts
 
-            hs_replace_data hs_replace_data (
-                .flw_hs(flw_hs),
-                .ldr_hs(internal_hs),
-                .data_i(data_t'(flw_hs.data))
-            );
+        hs_io #(.T(data_t)) flw_indirect_hs (.*);
+        hs_replace_data hs_replace_data (
+            .flw_hs(flw_hs),
+            .ldr_hs(flw_indirect_hs),
+            .data_i(data_t'(flw_hs.data))
+        );
+        // TODO: once aborts are removed, drop hs_register
+        //       as it just exists here to consume aborts.
+        hs_register hs_register (
+            .flw_hs(flw_indirect_hs),
+            .ldr_hs(internal_hs)
+        );
 
-            assign advance_flw = internal_hs.flag.good || internal_hs.flag.term;
-            assign advance_ldr = ldr_hs.flag.good || ldr_hs.flag.term;
+        assign advance_flw = internal_hs.flag.good;
+        assign advance_ldr = ldr_hs.flag.good;
 
-            typedef struct packed {
-                data_t data;
-                logic  close;  // verilator lint_off SYMRSVDWORD
-                logic  abort;  // verilator lint_on SYMRSVDWORD
-            } entry_long_s;
+        typedef struct packed {
+            data_t data;
+            logic  close;
+        } entry_s;
 
-            entry_long_s write;
-            assign write.data          = internal_hs.data;
-            assign write.close         = internal_hs.flag.exit;
-            assign write.abort         = internal_hs.flag.term;
-            assign write_entry         = write;
+        entry_s write;
+        assign write.data          = internal_hs.data;
+        assign write.close         = internal_hs.flag.exit;
+        assign write_entry         = write;
 
-            // LT/GTE Depth is used instead of NE/EQ to account for the 1 overflow abort slot
-            assign internal_fctl.ready = write_util < util_t'(Depth);
-            assign internal_fctl.pause = write_util >= util_t'(Depth);
+        assign internal_fctl.ready = write_util != util_t'(Depth);
+        assign internal_fctl.pause = write_util == util_t'(Depth);
 
-            entry_long_s read;
-            assign read           = read_entry;
-            assign ldr_hs.data    = `HS_CAST(ldr_hs, read.data);
-            assign ldr_lctl.start = ldr_valid;
-            assign ldr_lctl.pause = !ldr_valid;
-            assign ldr_lctl.close = read.close;
-            assign ldr_lctl.abort = read.abort && ldr_valid;
-        end
-        else begin : g_eat_aborts
-
-            hs_io #(.T(data_t)) flw_indirect_hs (.*);
-            hs_replace_data hs_replace_data (
-                .flw_hs(flw_hs),
-                .ldr_hs(flw_indirect_hs),
-                .data_i(data_t'(flw_hs.data))
-            );
-            hs_absorb_aborts hs_absorb_aborts (
-                .flw_hs(flw_indirect_hs),
-                .ldr_hs(internal_hs)
-            );
-
-            assign advance_flw = internal_hs.flag.good;
-            assign advance_ldr = ldr_hs.flag.good;
-
-            typedef struct packed {
-                data_t data;
-                logic  close;
-            } entry_short_s;
-
-            entry_short_s write;
-            assign write.data          = internal_hs.data;
-            assign write.close         = internal_hs.flag.exit;
-            assign write_entry         = write;
-
-            assign internal_fctl.ready = write_util != util_t'(Depth);
-            assign internal_fctl.pause = write_util == util_t'(Depth);
-
-            entry_short_s read;
-            assign read           = read_entry;
-            assign ldr_hs.data    = `HS_CAST(ldr_hs, read.data);
-            assign ldr_lctl.start = ldr_valid;
-            assign ldr_lctl.pause = !ldr_valid;
-            assign ldr_lctl.close = read.close;
-            assign ldr_lctl.abort = 1'b0;
-        end
+        entry_s read;
+        assign read           = read_entry;
+        assign ldr_hs.data    = `HS_CAST(ldr_hs, read.data);
+        assign ldr_lctl.start = ldr_valid;
+        assign ldr_lctl.pause = !ldr_valid;
+        assign ldr_lctl.close = read.close;
+        assign ldr_lctl.abort = 1'b0;
 
     endgenerate
 
@@ -142,7 +105,7 @@ module hs_fifo #(
     assign read_ptr_next = (read_ptr_reg == AddrMax) ? ptr_t'(0) : (read_ptr_reg + ptr_t'(1));
     assign read_ptr      = advance_ldr ? read_ptr_next : read_ptr_reg;
 
-    (* syn_ramstyle = "no_rw_check" *) entry_t fifo[SafeDepth];
+    (* syn_ramstyle = "no_rw_check" *) entry_t fifo[Depth];
     always_ff @(posedge clk) begin
         if (clk_en) begin
             if (advance_flw) begin
