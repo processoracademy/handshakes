47a97b70d3ae0f9e44f12eaf870bcabc26002efa
Quinn Unger <quinn.unger@pm.me>
2026-06-10 12:43:49 +0100

    update flake and add `slang-server` lsp config

--- flake.nix
+++ flake.nix
@@ -1,34 +1,79 @@
 {
-    description = "systemverilog handshakes library";
+  description = "systemverilog handshakes library";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        flake-parts.url = "github:hercules-ci/flake-parts";
-        fusesoc-flake.url = "github:Mop-u/fusesoc-flake";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    moppkgs.url = "github:Mop-u/moppkgs";
+    fusesoc-flake.url = "github:Mop-u/fusesoc-flake";
+  };
 
-    outputs =
-        inputs@{ flake-parts, ... }:
-        flake-parts.lib.mkFlake { inherit inputs; } {
-            imports = [ inputs.fusesoc-flake.flakeModule ];
-            systems = [ "x86_64-linux" ];
-            perSystem =
-                {
-                    config,
-                    self',
-                    inputs',
-                    pkgs,
-                    system,
-                    ...
-                }:
-                {
-                    fusesoc-project = {
-                        withVerilator = true;
-                        sources.local = "src";
-                        extraPackages = [
-                            pkgs.verible
-                        ];
-                    };
-                };
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forEachSystem = systems: f: builtins.foldl' (lib.recursiveUpdate) { } (map f systems);
+    in
+    (forEachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
+        inherit (inputs.moppkgs.packages.${system})
+          slang-server
+          ;
+        inherit (inputs.fusesoc-flake.packages.${system})
+          fusesocLib
+          mkFusesocCore
+          runFusesocCore
+          ;
+
+        slangConf = pkgs.writeText "server.json" (
+          builtins.toJSON {
+            flags = lib.concatStringsSep " " [
+              "-Weverything"
+              "-Wno-empty-output-connection"
+              "-DSIM_DEBUG"
+              "-DSV2V"
+              "-I ${./src/handshakes/base/rtl}"
+            ];
+            index = [
+              {
+                dirs = [ "src" ];
+                excludeDirs = [
+                  "build"
+                  ".direnv"
+                ];
+              }
+            ];
+          }
+        );
+
+      in
+      {
+        devShells.${system}.default =
+          (runFusesocCore (finalAttrs: {
+            core = fusesocLib.readYAML ./src/handshakes/handshakes/handshakes.core;
+            target = "default";
+            dependencies = [ ./src/handshakes ];
+            nativeBuildInputs = [
+              slang-server
+              pkgs.verible
+            ];
+          })).overrideAttrs
+            (
+              final: prev: {
+                shellHook =
+                  (prev.shellHook or "")
+                  + "\n"
+                  + ''
+                    mkdir -p .slang
+                    rm -f .slang/server.json
+                    ln -s ${slangConf} .slang/server.json
+                  '';
+              }
+            );
+      }
+    ));
 }
