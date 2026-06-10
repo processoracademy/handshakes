237bddea644e390e52c9c436a1a77a77e94ea4a3
Quinn Unger <quinn.unger@pm.me>
2026-06-10 16:53:07 +0100

    rework `hs_demux_mask` to use `hs_register` instead of forwarding buffer

--- src/handshakes/handshakes/rtl/hs_demux_mask.sv
+++ src/handshakes/handshakes/rtl/hs_demux_mask.sv
@@ -13,7 +13,6 @@
  *  mask_i - configuration mask with each bit corresponding to a ldr_hs handshake at the same index
  * 
  * Parameters:
- *  Bare       - set to 1'b1 to leave all the <hs_io> .data ports disconnected
  *  Handshakes - The number of handshakes in the ldr_hs <hs_io> handshake array
  * 
  * Quinn Unger 02/July/2025
@@ -32,14 +31,48 @@ module hs_demux_mask #(
     `HS_ASSERT_H(flw_hs, ldr_hs[0])
 
     typedef logic [Handshakes-1:0] mask_t;
-    mask_t set_mask;
-    mask_t set_mask_reg;
+    typedef logic [flw_hs.W-1:0] data_t;
+
+    typedef struct packed {
+        data_t data;
+        mask_t mask;
+    } register_s;
+
+    mask_t mask_reg;
     always_ff @(posedge clk) begin
         if (clk_en && flw_hs.flag.init) begin
-            set_mask_reg <= mask_i;
+            mask_reg <= mask_i;
         end
     end
-    assign set_mask = flw_hs.flag.init ? mask_i : set_mask_reg;
+
+    register_s unregistered;
+    assign unregistered.data = flw_hs.data;
+    assign unregistered.mask = (flw_hs.state == hs::MULTI) ? mask_reg : mask_i;
+    hs_io #(.T(register_s)) unregistered_hs (.*);
+    hs_replace_data hs_replace_data (
+        .flw_hs(flw_hs),
+        .ldr_hs(unregistered_hs),
+        .data_i(unregistered)
+    );
+
+    hs_io #(.T(register_s)) register_hs (.*);
+    hs_register hs_register_data (
+        .flw_hs(unregistered_hs),
+        .ldr_hs(register_hs)
+    );
+
+    wire  valid = register_hs.ldrv.req;
+    wire  last = register_hs.ldrv.last;
+
+    logic valid_raised;
+    _hs_monostable _hs_monostable (
+        .clk     (clk),
+        .clk_en  (clk_en),
+        .sync_rst(sync_rst),
+        .sense   (valid),
+        .mono    (valid_raised)
+    );
+    wire   fresh_data = valid_raised || (valid && register_hs.prev_flag.good);
 
     mask_t pending_reg;
     mask_t pending_clr;
@@ -50,35 +83,30 @@ module hs_demux_mask #(
         .clk       (clk),
         .clk_en    (clk_en),
         .sync_rst  (sync_rst),
-        .set_i     (set_mask),
-        .set_en_i  (flw_hs.flag.good),
+        .set_i     (register_hs.data.mask),
+        .set_en_i  (fresh_data),
         .clear_i   (pending_clr),
         .clear_en_i(1'b1),
         .vector_o  (pending_reg)
     );
 
-    mask_t     ldr_not_ready;
-    hs::fctl_s flw_fctl;
-    `HS_DRIVE_FLW(flw_hs, flw_fctl)
-    assign flw_fctl.ready = 1'b1;
-    assign flw_fctl.pause = |pending_reg;  // only high if we didn't get everything last cycle.
-    assign flw_fctl.block = |ldr_not_ready;
+    mask_t pending;
+    assign pending              = fresh_data ? register_hs.data.mask : pending_reg;
+    assign register_hs.fdrv.ack = ((pending & (~pending_clr)) == '0) && (register_hs.state != hs::BLOCK);
 
     genvar i;
     generate
         for (i = 0; i < Handshakes; i = i + 1) begin : g_connect_leaders
-            logic      pending;
             hs::lctl_s ldr_lctl;
             `HS_DRIVE_LDR(ldr_hs[i], ldr_lctl)
-
-            assign pending = pending_reg[i] || flw_hs.flag.good;  // forward flag.good to avoid handshake bubbles.
-            assign ldr_hs[i].data = flw_hs.data_stable;
-            assign ldr_lctl.start = flw_hs.flag.init && mask_i[i];
-            assign ldr_lctl.pause = !pending;
-            assign ldr_lctl.close = (flw_hs.flag.exit || (flw_hs.state == hs::BLOCK)) && pending;
-            assign ldr_lctl.abort = (flw_hs.state == hs::BLOCK) && !pending;
-            assign pending_clr[i] = ldr_hs[i].flag.good;
-            assign ldr_not_ready[i] = pending_reg[i] || (ldr_hs[i].state != hs::READY);
+            always_comb begin
+                ldr_hs[i].data = `HS_CAST(ldr_hs[i], register_hs.data.data);
+                ldr_lctl.start = pending[i] && valid;
+                ldr_lctl.pause = !(pending[i] && valid);
+                ldr_lctl.close = last;
+                ldr_lctl.abort = 1'b0;
+                pending_clr[i] = ldr_hs[i].flag.good;
+            end
         end
     endgenerate
 
