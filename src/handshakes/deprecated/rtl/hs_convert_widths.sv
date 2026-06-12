ab56d509760e342744b9351616cac980aab537fe
Quinn Unger <quinn.unger@pm.me>
2026-06-12 14:31:05 +0100

    Fixup hs convert widths (#28)
    * fixup `hs_convert_widths` to use newer handshake modules
    
    * hs_convert_widths: Add endianness toggle

--- src/handshakes/deprecated/rtl/hs_convert_widths.sv
+++ src/handshakes/deprecated/rtl/hs_convert_widths.sv
@@ -8,22 +8,22 @@
  * 
  * Data is moved assuming least significant bit/word is sent first.
  * 
- * Narrow words are not split up between wide word boundaries!
- * e.g. if the narrow width doesn't evenly divide into the wide width, the upper bits of the wide 
- * word are left unutilized, and the next fitting narrow word will be sent on the following wide.
- * This can be undesirable behaviour, so setting EnforceEvenFit will throw an error on size mismatch.
- * 
  * Quinn Unger 25/Oct/2023
 **/
 
 `include "hs_macro.sv"
 module hs_convert_widths #(
-    parameter logic EnforceEvenFit = 1'b0
+    parameter logic BigEndian = 1'b0
 ) (
     hs_io.flw flw_hs,
     hs_io.ldr ldr_hs
 );
     initial $warning("hs_convert_widths is deprecated!");
+
+    wire clk = flw_hs.clk;
+    wire clk_en = flw_hs.clk_en;
+    wire sync_rst = flw_hs.sync_rst;
+
     typedef enum {
         FollowerIsWider,
         LeaderIsWider,
@@ -41,163 +41,57 @@ module hs_convert_widths #(
     localparam integer Entries = ceil_div(WideWidth, NarrowWidth);
     localparam integer BufWidth = Entries * NarrowWidth;
     localparam integer BufOversize = BufWidth - WideWidth;
-    localparam integer PtrWidth = Entries > 1 ? $clog2(Entries) : 1;
+
+    typedef logic [$clog2(Entries+1)-1:0] entries_t;
 
 `ifdef SIM_DEBUG
     generate
-        if (EnforceEvenFit && (BufOversize > 0)) begin : g_enforce_even_fit_error
+        if (BufOversize > 0) begin : g_enforce_even_fit_error
             $fatal(0, "flw_hs width (%0d) and ldr_hs width (%0d) do not divide evenly!", flw_hs.W, ldr_hs.W);
         end
     endgenerate
 `endif
 
-    typedef logic unsigned [PtrWidth-1:0] ptr_t;
-    typedef logic unsigned [Entries-1:0][NarrowWidth-1:0] buf_t;
-    typedef logic [ldr_hs.W-1:0] ldr_t;
-    typedef logic [flw_hs.W-1:0] flw_t;
-
-    typedef struct packed {
-        ptr_t ptr;
-        logic valid;
-        logic init;
-        logic last;
-        logic block;
-    } state_s;
-
-    state_s state, state_next;
-
-    logic clk, clk_en, sync_rst;
-    assign clk      = flw_hs.clk;
-    assign clk_en   = flw_hs.clk_en;
-    assign sync_rst = flw_hs.sync_rst;
-
-    flw_t      flw_word;
-    ldr_t      ldr_word;
-
-    hs::fctl_s flw_fctl;
-    `HS_DRIVE_FLW(flw_hs, flw_fctl)
-
-    assign flw_word       = flw_t'(flw_hs.data);
-    assign flw_fctl.ready = !state.valid;
-    assign flw_fctl.pause = state.valid;
-    assign flw_fctl.block = state.block;
-
-    hs::lctl_s ldr_lctl;
-    `HS_DRIVE_LDR(ldr_hs, ldr_lctl)
-
-    assign ldr_hs.data = `HS_CAST(ldr_hs, ldr_word);
-    always_comb begin
-        ldr_lctl.start = state.init && state.valid;
-        ldr_lctl.pause = !state.valid;
-        unique case (WidthTest)
-            FollowerIsWider: ldr_lctl.close = state.last && state.valid && (state.ptr == ptr_t'(Entries - 1));
-            default:         ldr_lctl.close = (state.last && state.valid);
-        endcase
-        ldr_lctl.abort = state.last && !state.valid;
-    end
-
-    logic ptr_inc, ptr_rst;
-    logic valid_set, valid_clr;
-
-    always_comb begin
-        state_next.block = state.block;
-        state_next.init  = state.init;
-        state_next.last  = state.last;
-        state_next.valid = state.valid;
-        state_next.ptr   = ptr_rst ? ptr_t'(0) : (state.ptr + ptr_t'(ptr_inc));
-
-        // block is high from flw_hs.flag.init until ldr_hs.flag.done
-        if (ldr_hs.flag.done) begin
-            state_next.block = 1'b0;
-        end
-        else if (flw_hs.flag.init) begin
-            state_next.block = 1'b1;
-        end
-
-        // init needs to stay high from flw_hs.init to ldr_hs.init
-        if (ldr_hs.flag.init) begin
-            state_next.init = 1'b0;
-        end
-        else if (flw_hs.flag.init) begin
-            state_next.init = 1'b1;
-        end
-
-        // last stays high from flw_hs.exit to ldr_hs.done
-        if (ldr_hs.flag.done) begin
-            state_next.last = 1'b0;
-        end
-        else if (flw_hs.flag.exit) begin
-            state_next.last = 1'b1;
-        end
-
-        if (valid_clr) begin
-            state_next.valid = 1'b0;
-        end
-        else if (valid_set) begin
-            state_next.valid = 1'b1;
-        end
-    end
-
-    always_ff @(posedge clk) begin
-        if (sync_rst) begin
-            state <= '0;
-        end
-        else if (clk_en) begin
-            state <= state_next;
-        end
-    end
-
     generate
         case (WidthTest)
             FollowerIsWider: begin : g_wide_to_narrow
-                // ptr increments on ldr_hs.flag.good, with reset on flw_hs.flag.good + wraparound logic.
-                assign ptr_inc   = ldr_hs.flag.good;
-                assign ptr_rst   = flw_hs.flag.good || (state.ptr == ptr_t'(Entries - 1));
-
-                // valid is high from flw_hs.good until the final word has been read from the internal buffer
-                assign valid_set = flw_hs.flag.good;
-                assign valid_clr = ldr_hs.flag.good && (state.ptr == ptr_t'(Entries - 1));
-
-                buf_t buffer;
-                always_ff @(posedge clk) begin
-                    if (clk_en && flw_hs.flag.good) begin
-                        buffer <= BufWidth'(flw_word);
-                    end
-                end
-
-                assign ldr_word = buffer[state.ptr];
+                hs_io #(.T(logic [ldr_hs.W-1:0])) internal_hs (.*);
+                hs_serialize #(
+                    .WideW    (WideWidth),
+                    .BigEndian(BigEndian)
+                ) hs_serialize (
+                    .data_i   (flw_hs.data),
+                    .length_i (entries_t'(Entries)),
+                    .fprobe_i (flw_hs.fprobe),
+                    .fdrv_o   (flw_hs.fdrv),
+                    .narrow_hs(internal_hs)
+                );
+                hs_replace_data hs_replace_data (
+                    .flw_hs(internal_hs),
+                    .ldr_hs(ldr_hs),
+                    .data_i(internal_hs.data)
+                );
             end
             LeaderIsWider: begin : g_narrow_to_wide
-                // ptr increments on flw_hs.flag.good, with reset on flw_hs.flag.init + wraparound logic.
-                assign ptr_inc   = flw_hs.flag.good;
-                assign ptr_rst   = ldr_hs.flag.good;
-
-                // valid is high when the buffer is full and ready for one wide output
-                assign valid_set = flw_hs.flag.exit || (flw_hs.flag.good && (state.ptr == ptr_t'(Entries - 1)));
-                assign valid_clr = ldr_hs.flag.good;
-
-                buf_t buffer;
-                always_ff @(posedge clk) begin
-                    if (clk_en && flw_hs.flag.good) begin
-                        buffer[state.ptr] <= flw_word;
-                    end
-                end
-
-                assign ldr_word = ldr_t'(buffer);
+                logic [ldr_hs.W-1:0] ldr_data;
+                hs_deserialize #(
+                    .WideW    (WideWidth),
+                    .BigEndian(BigEndian)
+                ) hs_deserialize (
+                    .narrow_hs(flw_hs),
+                    .data_o   (ldr_data),
+                    .length_o (),
+                    .lprobe_i (ldr_hs.lprobe),
+                    .ldrv_o   (ldr_hs.ldrv)
+                );
+                assign ldr_hs.data = `HS_CAST(ldr_hs, ldr_data);
             end
             default:
             begin : g_equal_width
-                assign ptr_inc   = 1'b0;
-                assign ptr_rst   = 1'b1;
-
-                assign valid_set = flw_hs.flag.good;
-                assign valid_clr = ldr_hs.flag.good;
-
-                always_ff @(posedge clk) begin
-                    if (clk_en && flw_hs.flag.good) begin
-                        ldr_word <= flw_word;
-                    end
-                end
+                hs_register hs_register (
+                    .flw_hs(flw_hs),
+                    .ldr_hs(ldr_hs)
+                );
             end
         endcase
     endgenerate
