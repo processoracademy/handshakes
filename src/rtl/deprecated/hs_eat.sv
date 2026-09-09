187cbac5200911d47e47540bc10b7d77d816f5dd
Quinn Unger <quinn.unger@pm.me>
2026-09-09 16:08:50 +0100

    drop deprecated modules

--- src/rtl/deprecated/hs_eat.sv
+++
@@ -1,7 +0,0 @@
-`include "hs_macro.sv"
-module hs_eat (
-    hs_io.flw flw_hs
-);
-    initial $warning("hs_eat is deprecated! Do `assign flw_hs.fdrv = flw_hs.drive_flw(hs::FctlReady);` instead.");
-    assign flw_hs.fdrv = hs::drive_flw(flw_hs.state, hs::FctlReady);
-endmodule : hs_eat
